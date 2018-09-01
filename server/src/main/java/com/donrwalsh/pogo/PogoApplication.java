f7af9245916e5dea2a277806f59ec913c3add827
Don R Walsh <donrwalsh@gmail.com>
2018-09-01 10:37:35 -0500

    CommandLineRunner
    Main class set up to be able to execute commands when run. More exploration/troubleshooting helpers.

--- server/src/main/java/com/donrwalsh/pogo/PogoApplication.java
+++ server/src/main/java/com/donrwalsh/pogo/PogoApplication.java
@@ -1,12 +1,30 @@
 package com.donrwalsh.pogo;
 
+import com.donrwalsh.pogo.repository.PokemonRepository;
+import com.donrwalsh.pogo.repository.TypesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PogoApplication {
+public class PogoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private PokemonRepository pokemon;
+    @Autowired
+    private TypesRepository types;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PogoApplication.class, args);
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello there");
+    }
 }
