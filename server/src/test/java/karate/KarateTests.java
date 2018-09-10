19704f4c9f6e6fe7a95730521c80aebacee5f6d1
Don R Walsh <donrwalsh@gmail.com>
2018-09-10 10:56:46 -0500

    Attempt at omitting karate from build process

--- server/src/test/java/karate/KarateTests.java
+++ server/src/test/java/karate/KarateTests.java
@@ -1,8 +1,10 @@
 package karate;
 
 import com.intuit.karate.junit4.Karate;
+import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
+@CucumberOptions(tags = "~@ignore")
 @RunWith(Karate.class)
 public class KarateTests {
 }
