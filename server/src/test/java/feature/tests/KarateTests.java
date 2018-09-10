ff4023f3d5c7928265c80762c6a6c0749a7b22c4
Don R Walsh <donrwalsh@gmail.com>
2018-09-10 11:37:27 -0500

    Mixing around folder structure
    Let's see how Jenkins feels about this, but this should do the trick.

--- server/src/test/java/feature/tests/KarateTests.java
+++ server/src/test/java/feature/tests/KarateTests.java
@@ -1,10 +1,10 @@
-package karate;
+package feature.tests;
 
 import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@CucumberOptions(tags = "~@ignore")
+@CucumberOptions(tags = {"~@ignore"})
 @RunWith(Karate.class)
 public class KarateTests {
 }
